e8bf58d86504bd8d65b4b3335db32e31ccfc3965
baizhi958216 <1475289190@qq.com>
2023-01-11 13:28:30 +0800

    015: v-model Modifiers
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-2-15/app.js
@@ -0,0 +1,26 @@
+const vm = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            url: 'https://bing.com',
+            raw_url: '<a href="https://bing.com" target="_blank">Bing</a>',
+            age: 20
+        }
+    },
+    methods: {
+        // ES6 arrow function not work here because of Proxy 
+        // you want to use `this` keyword
+        fullName() {
+            return `${this.firstName} ${this.lastName}`
+        },
+        increment() {
+            this.age++
+        },
+        updateLastName(msg,event) {
+            console.log(msg);
+            
+            this.lastName = event.target.value
+        },
+    }
+}).mount('#app')
