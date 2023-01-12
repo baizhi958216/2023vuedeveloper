a109f0cfdc7d6ef404c712890143bf821629d56c
baizhi958216 <1475289190@qq.com>
2023-01-12 23:51:54 +0800

    04-001: Mounting the Vue Instance
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-04-01/app.js
@@ -0,0 +1,13 @@
+let vm = Vue.createApp({
+    data() {
+        return {
+            message: 'Hello world!'
+        }
+    }
+})
+
+vm.mount('#app')
+
+/* setTimeout(() => {
+    vm.mount('#app')
+}, 3000); */
