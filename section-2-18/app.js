7b9cc4476454c56fb9973718eaebef14bf2d1901
baizhi958216 <1475289190@qq.com>
2023-01-12 11:49:33 +0800

    018: Binding Classes
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-2-18/app.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+    data() {
+        return {
+            isPurple: false,
+            selectedColor: ''
+        }
+    },
+    computed: {
+        circle_classes() {
+            return { purple: this.isPurple }
+        }
+    }
+}).mount('#app')
