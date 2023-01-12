f727875188e5cd94afbd778217de721f381c8bfc
baizhi958216 <1475289190@qq.com>
2023-01-12 12:12:35 +0800

    019: Binding Styles
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-2-19/app.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+    data() {
+        return {
+            isPurple: false,
+            selectedColor: '',
+            size:150
+        }
+    },
+    computed: {
+        circle_classes() {
+            return { purple: this.isPurple }
+        }
+    }
+}).mount('#app')
