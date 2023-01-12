7342088b0277fadd822a8b8e036de094e3ad0b03
baizhi958216 <1475289190@qq.com>
2023-01-12 21:33:02 +0800

    022: List Rendering
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-2-22/app.js
@@ -0,0 +1,12 @@
+const vm = Vue.createApp({
+    data() {
+        return {
+            birds: ['Pigeons', 'Eagles', 'Doves', 'Parrots'],
+            people: [
+                { name: 'John', age: 20 },
+                { name: 'Rick', age: 18 },
+                { name: 'Amy', age: 33 }
+            ]
+        }
+    }
+}).mount('#app')
