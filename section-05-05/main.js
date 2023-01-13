05039a446517992d3768132a4cd4c5faf73cb3c4
baizhi958216 <1475289190@qq.com>
2023-01-14 02:02:44 +0800

    05-005: Understanding SASS
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-05-05/main.js
+++ section-05-05/main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.scss'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
@@ -10,7 +10,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello <span>Vite</span>!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
