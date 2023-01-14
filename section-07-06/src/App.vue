33cba00b12f21dc51a14f1216a4e5ae384b489af
baizhi958216 <1475289190@qq.com>
2023-01-15 01:43:17 +0800

    07-006: CSS and JavaScript Transitions
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-07-06/src/App.vue
+++ section-07-06/src/App.vue
@@ -18,7 +18,8 @@
   @before-leave="beforeLeave"
   @leave="leave" 
   @after-leave="afterLeave"
-  :css="false"
+  :css="true"
+  name="fade"
   >
     <h2 v-if="flag">Hey</h2>
   </transition>
@@ -36,16 +37,8 @@ export default {
     beforEnter(el) {
       console.log('before-enter event fired', el);
     },
-    enter(el, done) {
+    enter(el) {
       console.log('enter event fired', el);
-
-      const animation = el.animate([{ transform: "scale3d(0,0,0)" },{}],{
-        duration: 1000
-      });
-
-      animation.onfinish = () =>{
-        done()
-      }
     },
     afterEnter(el) {
       console.log('after-enter event fired', el);
@@ -53,16 +46,8 @@ export default {
     beforeLeave(el) {
       console.log('before-leave event fired', el);
     },
-    leave(el, done) {
+    leave(el) {
       console.log('leave event fired', el);
-
-      const animation = el.animate([{},{ transform: "scale3d(0,0,0)" }],{
-        duration: 1000
-      });
-
-      animation.onfinish = () =>{
-        done()
-      }
     },
     afterLeave(el) {
       console.log('after-leave event fired', el);
