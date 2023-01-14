bd0dca6a3d781ff4fcce73271a465c7352bcc92b
baizhi958216 <1475289190@qq.com>
2023-01-14 21:15:57 +0800

    06-011: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-06-11/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>Hey!</h3>
+
+  <button type="button" @click="age++">Update Age</button>
+
+  <greeting :age="age"></greeting>
+  <user :age="age"></user>
+</template>
+
+<script>
+import Greeting from './components/Greeting.vue';
+import User from './components/User.vue';
+export default {
+  name: 'App',
+  components: {
+    // Greeting: Greeting
+    
+    // ES2015 ↓
+    Greeting,
+    User
+  },
+  data() {
+    return {
+      age: 20
+    }
+  }
+}
+</script>
---
+++ section-06-11/src/components/User.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- 
+        The changes will not be reflected in other components.
+        components is not being able to hold on to those changes.
+        parent component updates will override the changes in the child component
+    -->
+    <button type="button" @click="age++">Update Age</button>
+    <p>The user is {{ age }} years old</p>
+</template>
+
+<script>
+export default {
+    name: 'User',
+    /* 
+        If the data inside the parent component changes,
+        then all children components will reflect that change.
+        
+        However, it doesn't necessarily work in the reverse.
+    */
+    props: ["age"]
+}
+</script>
