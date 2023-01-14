1def97a0cc74688748fa775cd95656be83533c1f
baizhi958216 <1475289190@qq.com>
2023-01-14 21:05:20 +0800

    06-010: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-06-10/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h3>Hey!</h3>
+  <greeting></greeting>
+  <user :age="age"></user>
+</template>
+
+<script>
+import Greeting from './components/Greeting.vue';
+import User from './components/User.vue';
+export default {
+  name: 'App',
+  components: {
+    // Greeting: Greeting
+    
+    // ES2015 ↓
+    Greeting,
+    User
+  },
+  data() {
+    return {
+      age: 20
+    }
+  }
+}
+</script>
