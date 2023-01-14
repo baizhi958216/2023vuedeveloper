73e771c332f3caae4d5ababf4e7f5a6ed3d41073
baizhi958216 <1475289190@qq.com>
2023-01-14 20:59:43 +0800

    06-009: Props
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-06-09/src/App.vue
+++ section-06-09/src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <p>Hey!</p>
+  <h3>Hey!</h3>
   <greeting></greeting>
+  <user :age="age"></user>
 </template>
 
 <script>
 import Greeting from './components/Greeting.vue';
+import User from './components/User.vue';
 export default {
   name: 'App',
   components: {
     // Greeting: Greeting
     
     // ES2015 ↓
-    Greeting
+    Greeting,
+    User
+  },
+  data() {
+    return {
+      age: 20
+    }
   }
 }
 </script>
---
+++ section-06-09/src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>The user is {{ age }} years old</p>
+</template>
+
+<script>
+export default {
+    name: 'User',
+    /* 
+        If the data inside the parent component changes,
+        then all children components will reflect that change.
+        
+        However, it doesn't necessarily work in the reverse.
+    */
+    props: ["age"]
+}
+</script>
