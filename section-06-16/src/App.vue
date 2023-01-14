cd80a1a05e00d47336d4421f0171eceeca315e28
baizhi958216 <1475289190@qq.com>
2023-01-14 22:59:40 +0800

    06-016: Dynamic Components
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-06-16/src/App.vue
+++ section-06-16/src/App.vue
@@ -1,7 +1,47 @@
-<template></template>
+<template>
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+
+  <!--  
+    Any data inside of it will not be available anymore 
+    if the component gets reintroduced on the page,
+    the data will be reset to its default state.
+  -->
+  <component :is="componentName"></component>
+
+  <!--  
+    keep-alive will make a component to persist.
+    It keeps the component that's inside of it alive, 
+    even if it shouldn't appear in the DOM, 
+    this will make sure the component lives in memory.
+
+    If the component needs to be added onto the page again,
+    vue will use the one in memory rather than create a new component.
+
+    unmounted lifecycle function doesn't get excuted anymore!
+    Instead activated and deactivated
+  -->
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
+</template>
 
 <script>
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Home,
+    About
+  },
+  data() {
+    return {
+      componentName: "Home"
+    }
+  }
 }
 </script>
--- section-06-16/src/components/Home.vue
+++ section-06-16/src/components/Home.vue
@@ -1,3 +1,16 @@
 <template>
     <h1>Home Page</h1>
 </template>
+<script>
+export default {
+    unmounted() {
+        console.log('Home component unmounted');
+    },
+    activated() {
+        console.log('Home component activated');
+    },
+    deactivated() {
+        console.log('Home component deactivated');
+    }
+}
+</script>
