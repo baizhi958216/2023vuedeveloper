92b610048f1ff86d3c46c355c7a1c1ad3094a07e
baizhi958216 <1475289190@qq.com>
2023-01-14 22:41:11 +0800

    06-014: Named Slots
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-06-15/src/components/Form.vue
+++ section-06-15/src/components/Form.vue
@@ -1,5 +1,18 @@
 <template>
     <form>
-        <slot>No form to render</slot>
+        <div class="help">
+            <slot name="help"></slot>
+        </div>
+        <div class="fields">
+            <slot name="fields"></slot>
+        </div>
+        <div class="buttons">
+            <slot name="buttons"></slot>
+        </div>
     </form>
+    <!--  
+        By having a slot with no name, any elements that aren't assigned
+        to a specific slot will be put here by default.
+    -->
+    <slot></slot>
 </template>
