c4a6e7bb2539d75f0ece583f67d5d5a3d2b55d74
baizhi958216 <1475289190@qq.com>
2023-01-15 01:39:24 +0800

    07-005: JavaScript Zoom Animation
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-07-05/src/App.vue
+++ section-07-05/src/App.vue
@@ -17,7 +17,9 @@
   @after-enter="afterEnter" 
   @before-leave="beforeLeave"
   @leave="leave" 
-  @after-leave="afterLeave">
+  @after-leave="afterLeave"
+  :css="false"
+  >
     <h2 v-if="flag">Hey</h2>
   </transition>
 </template>
@@ -36,7 +38,14 @@ export default {
     },
     enter(el, done) {
       console.log('enter event fired', el);
-      done()
+
+      const animation = el.animate([{ transform: "scale3d(0,0,0)" },{}],{
+        duration: 1000
+      });
+
+      animation.onfinish = () =>{
+        done()
+      }
     },
     afterEnter(el) {
       console.log('after-enter event fired', el);
@@ -46,7 +55,14 @@ export default {
     },
     leave(el, done) {
       console.log('leave event fired', el);
-      done()
+
+      const animation = el.animate([{},{ transform: "scale3d(0,0,0)" }],{
+        duration: 1000
+      });
+
+      animation.onfinish = () =>{
+        done()
+      }
     },
     afterLeave(el) {
       console.log('after-leave event fired', el);
