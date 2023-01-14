00e98879c5f1e3745b90b65f7e5423f3262f0519
baizhi958216 <1475289190@qq.com>
2023-01-15 01:14:31 +0800

    07-003: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-07-03/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <button type="button" @click="flag = !flag">Toggle</button>
+  
+  <transition name="fade" mode="out-in">
+    <h2 v-if="flag">Hello World!</h2>
+    <h2 v-else>Another Hello!</h2>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      flag: false
+    }
+  }
+}
+</script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.25s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.25s linear;
+  opacity: 0;
+}
+</style>
