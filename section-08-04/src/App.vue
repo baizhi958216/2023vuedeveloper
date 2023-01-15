3eee79498d03ce20d35df1cb8359e1632bf6ab12
baizhi958216 <1475289190@qq.com>
2023-01-15 12:03:13 +0800

    08-004: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-08-04/src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="ctr">
+    <questions v-if="questionsAnswered < questions.length" :questions="questions" :questionsAnswered="questionsAnswered"
+      @question-answered="questionAnswered" />
+    <result v-else />
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
+</template>
+
+<script>
+import Questions from './components/Questions.vue';
+import Result from './components/Result.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Questions,
+    Result
+  },
+  data() {
+    return {
+      questionsAnswered: 0,
+      totalCorrect: 0,
+      questions: [
+        {
+          q: "What is 2 + 2?",
+          answers: [
+            {
+              text: "4",
+              is_correct: true
+            },
+            {
+              text: "3",
+              is_correct: false
+            },
+            {
+              text: "Fish",
+              is_correct: false
+            }
+          ]
+        },
+        {
+          q: 'How many letters are in the word "Banana"?',
+          answers: [
+            {
+              text: "5",
+              is_correct: false
+            },
+            {
+              text: "7",
+              is_correct: false
+            },
+            {
+              text: "6",
+              is_correct: true
+            }
+          ]
+        },
+        {
+          q: "Find the missing letter: C_ke",
+          answers: [
+            {
+              text: "e",
+              is_correct: false
+            },
+            {
+              text: "a",
+              is_correct: true
+            },
+            {
+              text: "i",
+              is_correct: false
+            }
+          ]
+        }
+      ],
+      results: [
+        {
+          min: 0,
+          max: 2,
+          title: "Try again!",
+          desc: "Do a little more studying and you may succeed!"
+        },
+        {
+          min: 3,
+          max: 3,
+          title: "Wow, you're a genius!",
+          desc: "Studying has definitely paid off for you!"
+        }
+      ]
+    }
+  },
+  methods: {
+    questionAnswered(is_correct) {
+      if (is_correct) {
+        this.totalCorrect++
+        console.log(this.totalCorrect);
+      }
+
+      this.questionsAnswered++
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ section-08-04/src/components/Questions.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="questions-ctr">
+        <div class="progress">
+            <div class="bar" :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"></div>
+            <div class="status">{{ questionsAnswered }} out of {{ questions.length }} questions answered</div>
+        </div>
+        <div class="single-question" v-for="(question, qi) in questions" :key="question.q"
+            v-show="questionsAnswered === qi">
+            <div class="question">{{ question.q }}</div>
+            <div class="answers">
+                <div class="answer" v-for="answer in question.answers" :key="answer.text"
+                    @click.prevent="selectAnswer(answer.is_correct)">
+                    {{ answer.text }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['questions', 'questionsAnswered'],
+    emits: ['question-answered'],
+    methods: {
+        selectAnswer(is_correct) {
+            this.$emit('question-answered', is_correct)
+        }
+    }
+}
+</script>
