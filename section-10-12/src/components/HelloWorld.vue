1915afa2c47fa8a8f8d4796bfc0d15174d1573fc
baizhi958216 <1475289190@qq.com>
2023-01-18 00:04:31 +0800

    10-012: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-10-12/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      user: {
+        name: "John",
+        age: 25,
+      },
+      favorites: ["Pizza", "Marbles", "Birds"],
+    };
+  },
+}
+</script>
