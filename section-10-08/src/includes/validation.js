a9e779ad33fdbf5862805648d795e18453395c14
baizhi958216 <1475289190@qq.com>
2023-01-17 23:03:09 +0800

    10-008: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-10-08/src/includes/validation.js
@@ -0,0 +1,27 @@
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+} from "vee-validate";
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+} from "@vee-validate/rules";
+
+export default {
+  install(app) {
+    app.component("VeeForm", VeeForm);
+    app.component("VeeField", VeeField);
+    app.component("ErrorMessage", ErrorMessage);
+
+    defineRule("required", required);
+    defineRule("min", min);
+    defineRule("max", max);
+    defineRule("alphaSpaces", alphaSpaces);
+    defineRule("email", email);
+  },
+};
