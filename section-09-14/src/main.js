c2511a7bc9f28ce765118df7fff754cd77d9bd94
baizhi958216 <1475289190@qq.com>
2023-01-17 11:16:48 +0800

    09-014: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-09-14/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/base.css";
+import "./assets/main.css";
+
+const app = createApp(App);
+
+/*  
+    Pinia is registering a plugin with the Vue Developer tools,
+    Pinia package will add a new tool for interacting with the state.
+*/
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
