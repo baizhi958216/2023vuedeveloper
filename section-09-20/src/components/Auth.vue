a767ef26b06ae76f462a5b4b31dc0bd2fbb4b5e3
baizhi958216 <1475289190@qq.com>
2023-01-17 13:56:51 +0800

    09-020: Adding Tabs
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-09-20/src/components/Auth.vue
+++ section-09-20/src/components/Auth.vue
@@ -38,20 +38,32 @@
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
               <a
-                class="block rounded py-3 px-4 transition hover:text-white text-white bg-blue-600"
+                class="block rounded py-3 px-4 transition"
                 href="#"
+                @click.prevent="tab = 'login'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'login',
+                  'hover:text-blue-600': tab === 'register',
+                }"
                 >Login</a
               >
             </li>
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition" href="#"
+              <a
+                class="block rounded py-3 px-4 transition"
+                href="#"
+                @click.prevent="tab = 'register'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'register',
+                  'hover:text-blue-600': tab === 'login',
+                }"
                 >Register</a
               >
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -78,7 +90,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -161,10 +173,15 @@ import useModalStore from "@/stores/modal";
 
 export default {
   name: "Auth",
+  data() {
+    return {
+      tab: "login",
+    };
+  },
   computed: {
     ...mapState(useModalStore, ["hiddenClass"]),
     ...mapWritableState(useModalStore, {
-      modalVisibility: "isOpen"
+      modalVisibility: "isOpen",
     }),
   },
 };
