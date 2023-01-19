c6e9e01c6bc446a7819f3ead7c38bd56920f66bf
baizhi958216 <1475289190@qq.com>
2023-01-19 13:54:08 +0800

    10-017: Showing Alerts
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-10-17/src/components/Auth.vue
+++ section-10-17/src/components/Auth.vue
@@ -90,6 +90,13 @@
             </button>
           </form>
           <!-- Registration Form -->
+          <div
+            class="text-white text-center font-blod p-4 rounded mb-4"
+            v-if="reg_show_alert"
+            :class="reg_alert_variant"
+          >
+            {{ reg_alert_msg }}
+          </div>
           <vee-form
             v-show="tab === 'register'"
             :validation-schema="schema"
@@ -187,6 +194,7 @@
             <button
               type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              :disabled="reg_in_submission"
             >
               Submit
             </button>
@@ -222,6 +230,10 @@ export default {
       userData: {
         country: "USA",
       },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: "bg-blur-500",
+      reg_alert_msg: "Please wait! Your account is being created",
     };
   },
   computed: {
@@ -232,6 +244,14 @@ export default {
   },
   methods: {
     register(values) {
+      this.reg_show_alert = true;
+      this.reg_in_submission = true;
+      this.reg_alert_variant = "bg-blue-500";
+      this.reg_alert_msg = "Please wait! Your account is being created.";
+
+      this.reg_alert_variant = "bg-green-500";
+      this.reg_alert_msg = "Success! Your account has been created.";
+
       console.log(values);
     },
   },
