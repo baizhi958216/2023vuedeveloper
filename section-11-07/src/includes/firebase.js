c1a7b53d705056813932f15db6ccf1999c19221a
baizhi958216 <1475289190@qq.com>
2023-01-25 21:02:12 +0800

    11-007: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-11-07/src/includes/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDT5kNloiw7uhhBLdRHezeVNg3QqZDhx1s",
+  authDomain: "music-9a6d5.firebaseapp.com",
+  projectId: "music-9a6d5",
+  storageBucket: "music-9a6d5.appspot.com",
+  appId: "1:860898955430:web:145066f681bdd6c1b7e467",
+};
+initializeApp(firebaseConfig);
+
+export { getAuth, createUserWithEmailAndPassword };
