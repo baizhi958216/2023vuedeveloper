52fec2cc9cd7c4846904376eeb26ddcc6d73911a
baizhi958216 <1475289190@qq.com>
2023-01-26 22:25:32 +0800

    11-015: Persisting the User Authentication
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-11-15/src/App.vue
+++ section-11-15/src/App.vue
@@ -273,12 +273,23 @@
 <script>
 import AppHeader from "./components/Header.vue";
 import Auth from "./components/Auth.vue";
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
+import { getAuth } from "./includes/firebase";
 
 export default {
   name: "App",
   components: {
     AppHeader,
     Auth,
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
+  },
+  created() {
+    if (getAuth().currentUser) {
+      this.userLoggedIn = true;
+    }
   },
 };
 </script>
