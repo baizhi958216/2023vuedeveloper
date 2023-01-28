58efb4037c198f6997e65be44a844888319becff
baizhi958216 <1475289190@qq.com>
2023-01-28 17:34:10 +0800

    12-002: Reviewing the Router Configuration
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-12-02/src/router/index.js
+++ section-12-02/src/router/index.js
@@ -1,23 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+
+const routes = [];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/About.vue"),
-    },
-  ],
+  routes,
 });
 
 export default router;
