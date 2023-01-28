d700e094d7d18a3b3ec416aa9ad4cce0b0bada6c
baizhi958216 <1475289190@qq.com>
2023-01-28 20:39:36 +0800

    12-006: Custom Links
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-12-06/src/components/Header.vue
+++ section-12-06/src/components/Header.vue
@@ -3,15 +3,16 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link
-        class="text-white font-bold uppercase text-2xl mr-4"
-        to="/about"
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
         >Music</router-link
       >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
+          <li>
+            <router-link class="px-2 text-white" to="/about">About</router-link>
+          </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
@@ -20,7 +21,9 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" to="/manage"
+                >Manage</router-link
+              >
             </li>
             <li>
               <a
