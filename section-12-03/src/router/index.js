4a940ed52f21321b77be5a179aa119ea74ffb477
baizhi958216 <1475289190@qq.com>
2023-01-28 17:42:17 +0800

    12-003: Creating Routes
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-12-03/src/router/index.js
+++ section-12-03/src/router/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
