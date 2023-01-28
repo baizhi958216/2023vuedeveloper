1a1f3d40b6bf011a1b4d7c34c4671bbde20fb5c3
baizhi958216 <1475289190@qq.com>
2023-01-28 20:23:08 +0800

    12-006: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-12-06/src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- Header -->
+  <header id="header" class="bg-gray-700">
+    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
+      <!-- App Name -->
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        to="/about"
+        >Music</router-link
+      >
+
+      <div class="flex flex-grow items-center">
+        <!-- Primary Navigation -->
+        <ul class="flex flex-row mt-1">
+          <!-- Navigation Links -->
+          <li v-if="!userStore.userLoggedIn">
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
+              >Login / Register</a
+            >
+          </li>
+          <template v-else>
+            <li>
+              <a class="px-2 text-white" href="#">Manage</a>
+            </li>
+            <li>
+              <a
+                class="px-2 text-white"
+                href="#"
+                @click.prevent="userStore.signOut"
+                >Logout</a
+              >
+            </li>
+          </template>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+<script>
+import { mapStores } from "pinia";
+import useModalStore from "@/stores/modal";
+import useUserStore from "@/stores/user";
+
+export default {
+  name: "AppHeader",
+  computed: {
+    ...mapStores(useModalStore, useUserStore),
+  },
+  methods: {
+    toggleAuthModal() {
+      this.modalStore.isOpen = !this.modalStore.isOpen;
+      console.log(this.modalStore.isOpen);
+    },
+  },
+};
+</script>
