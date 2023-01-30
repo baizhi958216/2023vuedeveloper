448b7bed0f99957caf21fc06981dde495b628606
baizhi958216 <1475289190@qq.com>
2023-01-30 19:44:10 +0800

    12-007: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-12-07/src/includes/firebase.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { collection, setDoc, getFirestore, doc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDT5kNloiw7uhhBLdRHezeVNg3QqZDhx1s",
+  authDomain: "music-9a6d5.firebaseapp.com",
+  projectId: "music-9a6d5",
+  storageBucket: "music-9a6d5.appspot.com",
+  appId: "1:860898955430:web:145066f681bdd6c1b7e467",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const usersCollection = collection(db, "users");
+
+export {
+  getAuth,
+  createUserWithEmailAndPassword,
+  usersCollection,
+  setDoc,
+  doc,
+  db,
+  updateProfile,
+  signInWithEmailAndPassword,
+};
---
+++ section-12-07/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/manage",
+    component: Manage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
