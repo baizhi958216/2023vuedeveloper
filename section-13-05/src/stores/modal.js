adc6749f607295a58afd85241b2e8d1df5772360
baizhi958216 <1475289190@qq.com>
2023-02-01 22:53:52 +0800

    13-005: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-13-05/src/stores/modal.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+export default defineStore("modal", {
+  state: () => ({
+    isOpen: false,
+  }),
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? "hidden" : "";
+    },
+  },
+});
