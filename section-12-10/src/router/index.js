10e1b65bb1930dc1e607ea48c0bd22d08ff1a6ac
baizhi958216 <1475289190@qq.com>
2023-02-01 10:37:10 +0800

    12-010: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-12-10/src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
+
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: Home,
+  },
+  {
+    name: "about",
+    path: "/about",
+    component: About,
+  },
+  {
+    name: "manage",
+    path: "/manage-music",
+    component: Manage,
+  },
+  {
+    path: "/manage",
+    redirect: { name: "manage" },
+  },
+  {
+    // regular expression match any value
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: "text-yellow-500",
+});
+
+export default router;
