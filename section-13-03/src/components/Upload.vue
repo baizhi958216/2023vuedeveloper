d93f9b618fd1f31a9607495b376e500817f6ee11
baizhi958216 <1475289190@qq.com>
2023-02-01 22:44:58 +0800

    13-003: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-13-03/src/components/Upload.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+    <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+      <span class="card-title">Upload</span>
+      <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
+    </div>
+    <div class="p-6">
+      <!-- Upload Dropbox -->
+      <div
+        class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
+        :class="{ 'bg-green-400 border-green-400 border-solid': is_dragover }"
+        @drag.prevent.stop=""
+        @dragstart.prevent.stop=""
+        @dragend.prevent.stop="is_dragover = false"
+        @dragover.prevent.stop="is_dragover = true"
+        @dragenter.prevent.stop="is_dragover = true"
+        @dragleave.prevent.stop="is_dragover = false"
+        @drop.prevent.stop="upload"
+      >
+        <h5>Drop your files here</h5>
+      </div>
+      <hr class="my-6" />
+      <!-- Progess Bars -->
+      <div class="mb-4">
+        <!-- File Name -->
+        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
+          <!-- Inner Progress Bar -->
+          <div
+            class="transition-all progress-bar bg-blue-400"
+            style="width: 75%"
+          ></div>
+        </div>
+      </div>
+      <div class="mb-4">
+        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
+          <div
+            class="transition-all progress-bar bg-blue-400"
+            style="width: 35%"
+          ></div>
+        </div>
+      </div>
+      <div class="mb-4">
+        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
+          <div
+            class="transition-all progress-bar bg-blue-400"
+            style="width: 55%"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Upload",
+  data() {
+    return {
+      is_dragover: false,
+    };
+  },
+  methods: {
+    upload() {
+      this.is_dragover = false;
+    },
+  },
+};
+</script>
---
+++ section-13-03/src/router/index.js
@@ -0,0 +1,68 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
+
+import useUserStore from "@/stores/user";
+
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: Home,
+  },
+  {
+    name: "about",
+    path: "/about",
+    component: About,
+  },
+  {
+    name: "manage",
+    // alias: "/manage",
+    path: "/manage-music",
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log("Manage Route Guard");
+      next();
+    },
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  // redirect is better for search engines to index
+  // the new URLS as fast as possible
+  {
+    path: "/manage",
+    redirect: { name: "manage" },
+  },
+  {
+    // regular expression match any value
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: "text-yellow-500",
+});
+
+router.beforeEach((to, from, next) => {
+  // console.log(to.meta);
+
+  if (!to.meta.requiresAuth) {
+    // The router will not render the component until called 'next()' function
+    next();
+    return;
+  }
+
+  const store = useUserStore();
+  if (store.userLoggedIn) {
+    next();
+  } else {
+    next({ name: "home" });
+  }
+});
+
+export default router;
