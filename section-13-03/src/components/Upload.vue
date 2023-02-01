63c92277986dca4aac2e44a4ba8cb93a8027f84e
baizhi958216 <1475289190@qq.com>
2023-02-01 22:47:01 +0800

    13-003: Handling the File
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-13-03/src/components/Upload.vue
+++ section-13-03/src/components/Upload.vue
@@ -15,7 +15,7 @@
         @dragover.prevent.stop="is_dragover = true"
         @dragenter.prevent.stop="is_dragover = true"
         @dragleave.prevent.stop="is_dragover = false"
-        @drop.prevent.stop="upload"
+        @drop.prevent.stop="upload($event)"
       >
         <h5>Drop your files here</h5>
       </div>
@@ -63,8 +63,18 @@ export default {
     };
   },
   methods: {
-    upload() {
+    upload($event) {
       this.is_dragover = false;
+
+      const files = [...$event.dataTransfer.files];
+
+      files.forEach((file) => {
+        if (file.type !== "audio/mpeg") {
+          return;
+        }
+      });
+
+      console.log(files);
     },
   },
 };
