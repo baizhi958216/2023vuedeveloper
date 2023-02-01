6ca9c150bd388189df9eff844b375cd6c8ac8e6a
baizhi958216 <1475289190@qq.com>
2023-02-01 14:11:29 +0800

    12-015: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-12-15/src/router/index.js
@@ -0,0 +1,53 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
+
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: Home,
+  },
+  {
+    name: "about",
+    path: "/about",
+    component: About,
+  },
+  {
+    name: "manage",
+    // alias: "/manage",
+    path: "/manage-music",
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log("Manage Route Guard");
+      next();
+    },
+  },
+  // redirect is better for search engines to index
+  // the new URLS as fast as possible
+  {
+    path: "/manage",
+    redirect: { name: "manage" },
+  },
+  {
+    // regular expression match any value
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: "text-yellow-500",
+});
+
+router.beforeEach((to, from, next) => {
+  console.log("Global Guard");
+
+  // The router will not render the component until called 'next()' function
+  next();
+});
+
+export default router;
