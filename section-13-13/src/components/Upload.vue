088ce063cd4c7fbf9d4b22f25a656ba989611c6e
baizhi958216 <1475289190@qq.com>
2023-02-02 23:04:40 +0800

    13-013: Fallback Upload
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-13-13/src/components/Upload.vue
+++ section-13-13/src/components/Upload.vue
@@ -19,6 +19,7 @@
       >
         <h5>Drop your files here</h5>
       </div>
+      <input type="file" multiple @change="upload($event)" />
       <hr class="my-6" />
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
@@ -61,7 +62,9 @@ export default {
     upload($event) {
       this.is_dragover = false;
 
-      const files = [...$event.dataTransfer.files];
+      const files = $event.dataTransfer
+        ? [...$event.dataTransfer.files]
+        : [...$event.target.files];
 
       files.forEach((file) => {
         // const storageRef = storage.ref("songs"); //music-9a6d5.appspot.com/songs
