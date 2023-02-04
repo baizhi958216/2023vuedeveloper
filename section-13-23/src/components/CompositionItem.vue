78f680657ddba98d84640626f7072c12a070fa35
baizhi958216 <1475289190@qq.com>
2023-02-04 09:11:41 +0800

    13-023: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-13-23/src/components/CompositionItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="border border-gray-200 p-3 mb-4 rounded">
+    <div v-show="!showForm">
+      <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
+      <button
+        class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
+      >
+        <i class="fa fa-times"></i>
+      </button>
+      <button
+        class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+        @click.prevent="showForm = !showForm"
+      >
+        <i class="fa fa-pencil-alt"></i>
+      </button>
+    </div>
+    <div v-show="showForm">
+      <vee-form
+        :validation-schema="schema"
+        :initial-values="song"
+        @submit="edit"
+      >
+        <div class="mb-3">
+          <label class="inline-block mb-2">Song Title</label>
+          <vee-field
+            type="text"
+            name="modified_name"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            placeholder="Enter Song Title"
+          />
+          <ErrorMessage class="text-red-600" name="modified_name" />
+        </div>
+        <div class="mb-3">
+          <label class="inline-block mb-2">Genre</label>
+          <vee-field
+            type="text"
+            name="genre"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            placeholder="Enter Genre"
+          />
+          <ErrorMessage class="text-red-600" name="genre" />
+        </div>
+        <button
+          type="submit"
+          class="py-1.5 px-3 rounded text-white bg-green-600"
+        >
+          Submit
+        </button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+        >
+          Go Back
+        </button>
+      </vee-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CompositionItem",
+  props: {
+    song: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      showForm: false,
+      schema: {
+        modified_name: "required",
+        genre: "alphaSpaces",
+      },
+    };
+  },
+  methods: {
+    edit() {
+      console.log("Song edited");
+    },
+  },
+};
+</script>
