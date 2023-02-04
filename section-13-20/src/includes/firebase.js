cdf31f9115ba2e6ddbd6a3274572ec170faf9538
baizhi958216 <1475289190@qq.com>
2023-02-04 08:52:39 +0800

    13-020: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-13-20/src/includes/firebase.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import {
+  collection,
+  setDoc,
+  getFirestore,
+  doc,
+  addDoc,
+  getDocs,
+  where,
+  query,
+} from "firebase/firestore";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDT5kNloiw7uhhBLdRHezeVNg3QqZDhx1s",
+  authDomain: "music-9a6d5.firebaseapp.com",
+  projectId: "music-9a6d5",
+  storageBucket: "music-9a6d5.appspot.com",
+  appId: "1:860898955430:web:145066f681bdd6c1b7e467",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+const usersCollection = collection(db, "users");
+const songsCollection = collection(db, "songs");
+
+export {
+  getAuth,
+  createUserWithEmailAndPassword,
+  usersCollection,
+  setDoc,
+  doc,
+  db,
+  updateProfile,
+  signInWithEmailAndPassword,
+  storage,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+  getDownloadURL,
+  songsCollection,
+  addDoc,
+  where,
+  query,
+  getDocs,
+};
