0a6def6e03109ff51280c997cb858a958a035458
baizhi958216 <1475289190@qq.com>
2023-02-04 09:10:25 +0800

    13-022: Validating the Song Form
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-13-22/src/components/CompositionItem.vue
+++ section-13-22/src/components/CompositionItem.vue
@@ -15,22 +15,30 @@
       </button>
     </div>
     <div v-show="showForm">
-      <form>
+      <vee-form
+        :validation-schema="schema"
+        :initial-values="song"
+        @submit="edit"
+      >
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
-          <input
+          <vee-field
             type="text"
+            name="modified_name"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Song Title"
           />
+          <ErrorMessage class="text-red-600" name="modified_name" />
         </div>
         <div class="mb-3">
           <label class="inline-block mb-2">Genre</label>
-          <input
+          <vee-field
             type="text"
+            name="genre"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Genre"
           />
+          <ErrorMessage class="text-red-600" name="genre" />
         </div>
         <button
           type="submit"
@@ -44,7 +52,7 @@
         >
           Go Back
         </button>
-      </form>
+      </vee-form>
     </div>
   </div>
 </template>
@@ -60,7 +68,16 @@ export default {
   data() {
     return {
       showForm: false,
+      schema: {
+        modified_name: "required",
+        genre: "alphaSpaces",
+      },
     };
+  },
+  methods: {
+    edit() {
+      console.log("Song edited");
+    },
   },
 };
 </script>
