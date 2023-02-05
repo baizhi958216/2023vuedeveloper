5f1e121bf1ed89e1e2c307576a3f7a5dd7c07cdf
baizhi958216 <1475289190@qq.com>
2023-02-05 22:21:25 +0800

    14-004: Path Parameters
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-14-04/src/router/index.js
+++ section-14-04/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Manage from "@/views/Manage.vue";
+import Song from "@/views/Song.vue";
 
 import useUserStore from "@/stores/user";
 
@@ -34,6 +35,11 @@ const routes = [
   {
     path: "/manage",
     redirect: { name: "manage" },
+  },
+  {
+    name: "song",
+    path: "/song/:id",
+    component: Song,
   },
   {
     // regular expression match any value
---
+++ section-14-04/src/views/Song.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    {{ $route.params.id }}
+  </div>
+</template>
+<script>
+export default {
+  name: "Song",
+};
+</script>
