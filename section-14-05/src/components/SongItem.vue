08463368c70b90d8e9a471fc5e79ca1265b93ad2
baizhi958216 <1475289190@qq.com>
2023-02-05 22:54:36 +0800

    14-005: Creating the Song Template
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-14-05/src/components/SongItem.vue
+++ section-14-05/src/components/SongItem.vue
@@ -3,9 +3,11 @@
     class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
   >
     <div>
-      <a href="#" class="font-bold block text-gray-600">{{
-        song.modified_name
-      }}</a>
+      <router-link
+        :to="{ name: 'song', params: { id: song.docID } }"
+        class="font-bold block text-gray-600"
+        >{{ song.modified_name }}</router-link
+      >
       <span class="text-gray-500 text-sm">{{ song.display_name }}</span>
     </div>
 
