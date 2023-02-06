e00fc6ca338355a01909ba99b105237d0f878602
baizhi958216 <1475289190@qq.com>
2023-02-06 23:33:46 +0800

    14-009: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-14-09/src/views/Song.vue
@@ -0,0 +1,210 @@
+<template>
+  <!-- Music Header -->
+  <section class="w-full mb-8 py-14 text-center text-white relative">
+    <div
+      class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
+      style="background-image: url(/assets/img/song-header.png)"
+    ></div>
+    <div class="container mx-auto flex items-center">
+      <!-- Play/Pause Button -->
+      <button
+        type="button"
+        class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
+      >
+        <i class="fas fa-play"></i>
+      </button>
+      <div class="z-50 text-left ml-8">
+        <!-- Song Info -->
+        <div class="text-3xl font-bold">{{ song.modified_name }}</div>
+        <div>{{ song.genre }}</div>
+      </div>
+    </div>
+  </section>
+  <!-- Form -->
+  <section class="container mx-auto mt-6">
+    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+        <!-- Comment Count -->
+        <span class="card-title">Comments (15)</span>
+        <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
+      </div>
+      <div class="p-6">
+        <div
+          class="text-white text-center font-blod p-4 mb-4"
+          v-if="comment_show_alert"
+          :class="comment_alert_variant"
+        >
+          {{ comment_alert_message }}
+        </div>
+        <vee-form
+          :validation-schema="schema"
+          @submit="addComment"
+          v-if="userLoggedIn"
+        >
+          <vee-field
+            as="textarea"
+            name="comment"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
+            placeholder="Your comment here..."
+          ></vee-field>
+          <ErrorMessage class="text-red-600" name="comment" />
+          <button
+            type="submit"
+            class="py-1.5 px-3 rounded text-white bg-green-600 block"
+            :disable="comment_in_submission"
+          >
+            Submit
+          </button>
+        </vee-form>
+        <!-- Sort Comments -->
+        <select
+          class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        >
+          <option value="1">Latest</option>
+          <option value="2">Oldest</option>
+        </select>
+      </div>
+    </div>
+  </section>
+  <!-- Comments -->
+  <ul class="container mx-auto">
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+  </ul>
+</template>
+<script>
+import {
+  songsCollection,
+  getDoc,
+  doc,
+  getAuth,
+  commentsCollection,
+  addDoc,
+} from "@/includes/firebase";
+import { mapState } from "pinia";
+import useUserStore from "@/stores/user";
+export default {
+  name: "Song",
+  data() {
+    return {
+      song: {},
+      schema: {
+        comment: "required|min:3",
+      },
+      comment_in_submission: false,
+      comment_show_alert: false,
+      comment_alert_variant: "bg-blue-500",
+      comment_alert_message: "Please wait! Your comment is being submitted.",
+    };
+  },
+  computed: {
+    ...mapState(useUserStore, ["userLoggedIn"]),
+  },
+  async created() {
+    const docRef = doc(songsCollection, this.$route.params.id);
+    const docSnapshot = await getDoc(docRef);
+
+    if (!docSnapshot.exists()) {
+      this.$router.push({ name: "home" });
+      return;
+    }
+
+    this.song = docSnapshot.data();
+  },
+  methods: {
+    async addComment(values, { resetForm }) {
+      this.comment_in_submission = true;
+      this.comment_show_alert = true;
+      this.comment_alert_variant = "bg-blue-500";
+      this.comment_alert_message =
+        "Please wait! Your comment is being submitted";
+
+      const comment = {
+        content: values.comment,
+        datePost: new Date().toString(),
+        sid: this.$route.params.id,
+        name: getAuth().currentUser.displayName,
+        uid: getAuth().currentUser.uid,
+      };
+
+      await addDoc(commentsCollection, comment);
+
+      this.comment_in_submission = false;
+      this.comment_alert_variant = "bg-green-500";
+      this.comment_alert_message = "Comment added!";
+
+      resetForm();
+    },
+  },
+};
+</script>
