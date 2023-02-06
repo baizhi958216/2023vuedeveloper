b54b59b2d5e9d6798f655205e3f0555f74a98e24
baizhi958216 <1475289190@qq.com>
2023-02-06 23:56:15 +0800

    14-010: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-14-10/src/components/SongItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li
+    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
+  >
+    <div>
+      <router-link
+        :to="{ name: 'song', params: { id: song.docID } }"
+        class="font-bold block text-gray-600"
+        >{{ song.modified_name }}</router-link
+      >
+      <span class="text-gray-500 text-sm">{{ song.display_name }}</span>
+    </div>
+
+    <div class="text-gray-600 text-lg">
+      <span class="comments">
+        <i class="fa fa-comments text-gray-600"></i>
+        {{ song.comment_count }}
+      </span>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "SongItem",
+  props: ["song"],
+};
+</script>
