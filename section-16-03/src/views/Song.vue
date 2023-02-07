153fa76efcd76b51ea9d0fdf9f586642abefef37
baizhi958216 <1475289190@qq.com>
2023-02-07 21:21:34 +0800

    16-003: Formatting and Pluralization
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-16-03/src/views/Song.vue
+++ section-16-03/src/views/Song.vue
@@ -32,7 +32,13 @@
       >
         <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
           <!-- Comment Count -->
-          <span class="card-title">Comments ({{ song.comment_count }})</span>
+          <span class="card-title">
+            {{
+              $tc("song.comment_count", song.comment_count, {
+                count: song.comment_count,
+              })
+            }}
+          </span>
           <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
         </div>
         <div class="p-6">
