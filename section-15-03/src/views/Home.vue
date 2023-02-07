f2730573b7f0c6e81a0a6c5af343968479f183d0
baizhi958216 <1475289190@qq.com>
2023-02-07 18:47:19 +0800

    15-003: Passing Values to Directives
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-15-03/src/views/Home.vue
+++ section-15-03/src/views/Home.vue
@@ -29,7 +29,10 @@
       <div
         class="bg-white rounded border border-gray-200 relative flex flex-col"
       >
-        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon>
+        <div
+          class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+          v-icon="'headphones-alt'"
+        >
           <span class="card-title">Songs</span>
           <!-- Icon -->
         </div>
