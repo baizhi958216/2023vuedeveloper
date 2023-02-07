994ae5adc6b9997564d337f2c33d357fd1bc142a
baizhi958216 <1475289190@qq.com>
2023-02-07 13:20:19 +0800

    14-019: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-14-19/src/stores/player.js
@@ -0,0 +1,58 @@
+import { defineStore } from "pinia";
+import { Howl } from "howler";
+import helper from "@/includes/helper";
+
+export default defineStore("player", {
+  state: () => ({
+    current_song: {},
+    sound: {},
+    seek: "00:00",
+    duration: "00:00",
+  }),
+  actions: {
+    async newSong(song) {
+      this.current_song = song;
+
+      this.sound = new Howl({
+        src: [song.url],
+        // By default, howler request the audio file with an AJAX request
+        // That will result in an origin policy error.
+        // set html5 to true, the library will switch to the HTML5 API to retrieve audio files.
+        html5: true,
+      });
+
+      this.sound.play();
+
+      this.sound.on("play", () => {
+        requestAnimationFrame(this.progress);
+      });
+    },
+    async toggleAudio() {
+      if (!this.sound.playing) {
+        return;
+      }
+
+      if (this.sound.playing()) {
+        this.sound.pause();
+      } else {
+        this.sound.play();
+      }
+    },
+    progress() {
+      this.seek = helper.formatTime(this.sound.seek());
+      this.duration = helper.formatTime(this.sound.duration());
+
+      if (this.sound.playing()) {
+        requestAnimationFrame(this.progress);
+      }
+    },
+  },
+  getters: {
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+      return false;
+    },
+  },
+});
