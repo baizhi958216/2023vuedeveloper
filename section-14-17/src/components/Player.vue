7aa687c82b40c39a7a9c8ecd2804bcf9bfe4dfec
baizhi958216 <1475289190@qq.com>
2023-02-07 12:51:42 +0800

    14-017: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-14-17/src/components/Player.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- Player -->
+  <div class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full">
+    <!-- Track Info -->
+    <div class="text-center">
+      <span class="song-title font-bold">Song Title</span> by
+      <span class="song-artist">Artist</span>
+    </div>
+    <div class="flex flex-nowrap gap-4 items-center">
+      <!-- Play/Pause Button -->
+      <button type="button" @click.prevent="toggleAudio">
+        <i
+          class="fa text-gray-500 text-xl"
+          :class="{ 'fa-play': !playing, 'fa-pause': playing }"
+        ></i>
+      </button>
+      <!-- Current Position -->
+      <div class="player-currenttime">00:00</div>
+      <!-- Scrub Container  -->
+      <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
+        <!-- Player Ball -->
+        <span
+          class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg"
+          style="left: 50%"
+        >
+          <i class="fas fa-circle"></i>
+        </span>
+        <!-- Player Progress Bar-->
+        <span
+          class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
+          style="width: 50%"
+        ></span>
+      </div>
+      <!-- Duration -->
+      <div class="player-duration">03:06</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "pinia";
+import usePlayerStore from "@/stores/player";
+export default {
+  name: "Player",
+  methods: {
+    ...mapActions(usePlayerStore, ["toggleAudio"]),
+  },
+  computed: {
+    ...mapState(usePlayerStore, ["playing"]),
+  },
+};
+</script>
