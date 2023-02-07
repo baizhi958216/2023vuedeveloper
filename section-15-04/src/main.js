4a957bf11dc52750581b7e4f69cc96af0b9f0940
baizhi958216 <1475289190@qq.com>
2023-02-07 19:02:30 +0800

    15-004: Create Vite
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-15-04/src/main.js
@@ -0,0 +1,32 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import VeeValidatePlugin from "./includes/validation";
+import { getAuth } from "./includes/firebase";
+
+import Icon from "./directives/icon";
+
+import "./assets/base.css";
+import "./assets/main.css";
+
+let app;
+
+getAuth().onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
+
+    /*  
+              Pinia is registering a plugin with the Vue Developer tools,
+              Pinia package will add a new tool for interacting with the state.
+          */
+    app.use(createPinia());
+    app.use(router);
+    app.use(VeeValidatePlugin);
+    app.directive("icon", Icon);
+
+    app.mount("#app");
+  }
+});
