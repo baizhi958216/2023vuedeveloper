dcbb5d7e95616b0fe131900f65b0b0adefaab5cd
baizhi958216 <1475289190@qq.com>
2023-02-07 18:23:31 +0800

    15-001: Introduction to Directives
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-15-01/src/directives/icon.js
@@ -0,0 +1,17 @@
+export default {
+  /* 
+    Lifecycle Functions
+    beforeMount: called when the directive is first bound to the element and before parent component is mounted
+    mounted: call when the directive is mounted to the document
+    beforeUpdate: call before the directive is updated
+    updated: call after the directive is updated
+    beforeUnmount: call before a directive is unmounted from the document
+    unmounted: called after a directive is unmounted from document
+
+    Hook Arguments
+    el: the element the binding sits on
+    binding: an object which contains the arguments that are passed into the hooks
+    vnode: allows you to refer directly to the node in the virtual DOM if you need to
+    prevNode: The previous vnode object before the directive was updated (applies to beforeUpdate and updated hooks only)
+    */
+};
