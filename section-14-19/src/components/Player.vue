9b86e65385fa1cf1c428af435312d77ef4a7a3c6
baizhi958216 <1475289190@qq.com>
2023-02-07 13:36:56 +0800

    14-019: Player Progress Bar
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-14-19/src/components/Player.vue
+++ section-14-19/src/components/Player.vue
@@ -2,9 +2,10 @@
   <!-- Player -->
   <div class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full">
     <!-- Track Info -->
-    <div class="text-center">
-      <span class="song-title font-bold">Song Title</span> by
-      <span class="song-artist">Artist</span>
+    <div class="text-center" v-if="current_song.modified_name">
+      <span class="song-title font-bold">{{ current_song.modified_name }}</span>
+      by
+      <span class="song-artist">{{ current_song.display_name }}</span>
     </div>
     <div class="flex flex-nowrap gap-4 items-center">
       <!-- Play/Pause Button -->
@@ -21,14 +22,14 @@
         <!-- Player Ball -->
         <span
           class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg"
-          style="left: 50%"
+          :style="{ left: playerProgress }"
         >
           <i class="fas fa-circle"></i>
         </span>
         <!-- Player Progress Bar-->
         <span
           class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
-          style="width: 50%"
+          :style="{ width: playerProgress }"
         ></span>
       </div>
       <!-- Duration -->
@@ -46,7 +47,13 @@ export default {
     ...mapActions(usePlayerStore, ["toggleAudio"]),
   },
   computed: {
-    ...mapState(usePlayerStore, ["playing", "duration", "seek"]),
+    ...mapState(usePlayerStore, [
+      "playing",
+      "duration",
+      "seek",
+      "playerProgress",
+      "current_song",
+    ]),
   },
 };
 </script>
--- section-14-19/src/stores/player.js
+++ section-14-19/src/stores/player.js
@@ -8,9 +8,13 @@ export default defineStore("player", {
     sound: {},
     seek: "00:00",
     duration: "00:00",
+    playerProgress: "0%",
   }),
   actions: {
     async newSong(song) {
+      if (this.sound instanceof Howl) {
+        this.sound.unload();
+      }
       this.current_song = song;
 
       this.sound = new Howl({
@@ -41,6 +45,10 @@ export default defineStore("player", {
     progress() {
       this.seek = helper.formatTime(this.sound.seek());
       this.duration = helper.formatTime(this.sound.duration());
+
+      this.playerProgress = `${
+        (this.sound.seek() / this.sound.duration()) * 100
+      }%`;
 
       if (this.sound.playing()) {
         requestAnimationFrame(this.progress);
