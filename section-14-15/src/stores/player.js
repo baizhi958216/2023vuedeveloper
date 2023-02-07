c70fc03b21e628fa3c2a945e5c78de60cd9732e9
baizhi958216 <1475289190@qq.com>
2023-02-07 12:33:16 +0800

    14-015: Playing Audio
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-14-15/src/stores/player.js
+++ section-14-15/src/stores/player.js
@@ -1,11 +1,23 @@
 import { defineStore } from "pinia";
+import { Howl } from "howler";
 export default defineStore("player", {
   state: () => ({
     current_song: {},
+    sound: {},
   }),
   actions: {
     async newSong(song) {
       this.current_song = song;
+
+      this.sound = new Howl({
+        src: [song.url],
+        // By default, howler request the audio file with an AJAX request
+        // That will result in an origin policy error.
+        // set html5 to true, the library will switch to the HTML5 API to retrieve audio files.
+        html5: true,
+      });
+
+      this.sound.play();
     },
   },
 });
