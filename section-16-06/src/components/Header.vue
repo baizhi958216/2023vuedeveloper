651870322a262ac7754eddd29be8a354fab0e881
baizhi958216 <1475289190@qq.com>
2023-02-08 17:57:39 +0800

    16-006: Changing Locales
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-16-06/src/components/Header.vue
+++ section-16-06/src/components/Header.vue
@@ -37,6 +37,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">
+              {{ currentLocale }}
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -50,6 +57,9 @@ export default {
   name: "AppHeader",
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "zh" ? "Chinese" : "English";
+    },
   },
   methods: {
     toggleAuthModal() {
@@ -62,6 +72,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: "home" });
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "zh" ? "en" : "zh";
     },
   },
 };
