f49d38f9716129c75f4f34318c83f544855fdb81
baizhi958216 <1475289190@qq.com>
2023-02-08 17:22:51 +0800

    16-005: Translating HTML with Component Interpolation
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-16-05/src/components/RegisterForm.vue
+++ section-16-05/src/components/RegisterForm.vue
@@ -93,7 +93,9 @@
         value="1"
         class="w-4 h-4 float-left -ml-6 mt-1 rounded"
       />
-      <label class="inline-block">Accept terms of service</label>
+      <i18n-t class="inline-block" keypath="register.accept" tag="label">
+        <a href="#">{{ $t("register.tos") }}</a>
+      </i18n-t>
       <ErrorMessage class="text-red-600 block" name="tos" />
     </div>
     <button
