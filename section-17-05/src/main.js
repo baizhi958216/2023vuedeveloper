d7139015cdac696f5d1e047ae348ee31a5a13696
baizhi958216 <1475289190@qq.com>
2023-02-09 22:38:40 +0800

    17-005: Offline Support with Service Workers
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-17-05/src/main.js
+++ section-17-05/src/main.js
@@ -9,9 +9,12 @@ import { getAuth } from "./includes/firebase";
 
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
+import { registerSW } from "virtual:pwa-register";
 
 import "./assets/base.css";
 import "./assets/main.css";
+
+registerSW({ immediate: true });
 
 let app;
 
