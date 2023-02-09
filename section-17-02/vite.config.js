239aa3aea0e967c1e45e9baa09ad8f89a4619eb1
baizhi958216 <1475289190@qq.com>
2023-02-09 18:27:39 +0800

    17-002: The Manifest File
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-17-02/vite.config.js
+++ section-17-02/vite.config.js
@@ -1,14 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enable: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
