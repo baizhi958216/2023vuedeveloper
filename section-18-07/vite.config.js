64521caa6b489d89e51e1fffbd2987c717921a82
baizhi958216 <1475289190@qq.com>
2023-02-11 21:52:05 +0800

    18-007: Rollup Visualizer
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-18-07/vite.config.js
+++ section-18-07/vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,6 +29,7 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,png,jpg,}"],
       },
     }),
+    visualizer({ open: true }),
   ],
   resolve: {
     alias: {
