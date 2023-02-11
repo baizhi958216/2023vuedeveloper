3a370e195eea51f1da4a606e7cdf6572e5cae3a7
baizhi958216 <1475289190@qq.com>
2023-02-11 21:28:44 +0800

    18-005: Progress Bar
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-18-05/src/main.js
+++ section-18-05/src/main.js
@@ -11,11 +11,15 @@ import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
 import { registerSW } from "virtual:pwa-register";
 import GlobalComponents from "./includes/_globals";
+import progressBar from "./includes/progress-bar";
 
 import "./assets/base.css";
 import "./assets/main.css";
+import "nprogress/nprogress.css";
 
 registerSW({ immediate: true });
+
+progressBar(router);
 
 let app;
 
