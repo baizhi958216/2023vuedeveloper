bcd444ea7c2857f973f38394128fb88b4114e842
baizhi958216 <1475289190@qq.com>
2023-02-11 21:20:54 +0800

    18-004: Dynamic Route Imports
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-18-04/src/router/index.js
+++ section-18-04/src/router/index.js
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/views/Home.vue";
-import About from "@/views/About.vue";
-import Manage from "@/views/Manage.vue";
-import Song from "@/views/Song.vue";
-
+const Home = () => import("@/views/Home.vue");
+const About = () => import("@/views/About.vue");
+const Manage = () => import("@/views/Manage.vue");
+const Song = () => import("@/views/Song.vue");
 import useUserStore from "@/stores/user";
 
 const routes = [
