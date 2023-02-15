65f7d57da6c52a8cfb724ec875844cfca5e605dc
baizhi958216 <1475289190@qq.com>
2023-02-15 23:52:38 +0800

    20-006: Testing the Inner Content
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-20-06/src/components/__tests__/about.spec.js
+++ section-20-06/src/components/__tests__/about.spec.js
@@ -1,8 +1,13 @@
 import About from "@/views/About.vue";
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 
 describe("About.vue", () => {
   test("renders inner test", () => {
-    const wrapper = mount(About);
+    /* const wrapper = mount(About, {
+      shallow: true,
+    }); */
+    const wrapper = shallowMount(About);
+
+    expect(wrapper.text()).toContain("关于");
   });
 });
