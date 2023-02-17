e93429beb0b92fdb9135132532ea8b69c01bbf54
baizhi958216 <1475289190@qq.com>
2023-02-17 21:33:23 +0800

    20-018: Import Resources
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-21-02/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="scrollPosition"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 3000px;
+  position: relative;
+}
+
+.scrollPosition {
+  position: fix;
+  left: 10px;
+  right: 10px;
+}
+</style>
