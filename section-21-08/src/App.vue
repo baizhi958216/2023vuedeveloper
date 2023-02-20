18b31c66995adf53da67b74505a8a0ba0bd2f379
baizhi958216 <1475289190@qq.com>
2023-02-20 17:50:25 +0800

    21-008: Import Resources
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-21-08/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>{{ num }}</div>
+  <p>{{ double }}</p>
+  <button type="button" @click.prevent="increment">Click Me</button>
+
+  <p>{{ name }}</p>
+
+  <p>
+    <input type="text" v-model="phrase" />
+  </p>
+  <P>{{ reversedPhrase }}</P>
+
+  <app-alert :user="user" />
+</template>
+
+<script>
+import {
+  ref,
+  reactive,
+  toRefs,
+  watchEffect,
+  watch,
+  computed,
+  onBeforeMount,
+  onMounted,
+} from "vue";
+import AppAlert from "@/components/Alert.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppAlert,
+  },
+  setup() {
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("onMounted");
+    });
+
+    let num = ref(0);
+
+    console.log(num);
+
+    function increment() {
+      num.value++;
+    }
+
+    const double = computed(() => {
+      return num.value * 2;
+    });
+
+    const user = reactive({
+      name: "John",
+      age: 20,
+    });
+
+    setTimeout(() => {
+      user.name = "Luis";
+    }, 3000);
+
+    const phrase = ref("");
+    const reversedPhrase = ref("");
+
+    /* watch([phrase],([newVal,oldVal]) => {
+      reversedPhrase.value = phrase.value.split("").reverse().join("");
+    }); */
+
+    watchEffect(() => {
+      reversedPhrase.value = phrase.value.split("").reverse().join("");
+    });
+
+    return {
+      num,
+      increment,
+      ...toRefs(user),
+      phrase,
+      reversedPhrase,
+      double,
+      user,
+    };
+  },
+};
+</script>
