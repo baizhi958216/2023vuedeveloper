a7ff5a059d62f611ced0e56360f165708a0d0166
baizhi958216 <1475289190@qq.com>
2023-02-21 00:52:40 +0800

    22-006: Import Resources
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-22-06/src/components/Modal.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="modal" :style="{ display: show ? 'block' : 'none' }">
+    <!-- <div
+      class="modal-dialog"
+      style="z-index: 2000"
+      @keydown.esc="close"
+      tabindex="0"
+      ref="modal"
+    > -->
+    <div class="modal-dialog" style="z-index: 2000">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Awesome Title</h5>
+          <button type="button" class="close" @click="close">
+            <span>&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Awesome content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop show"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppModal",
+  props: ["show"],
+  methods: {
+    close() {
+      this.$emit("hide");
+    },
+    handler(e) {
+      if (e.code === "Escape" && this.show) {
+        this.close();
+      }
+    },
+  },
+  created() {
+    document.addEventListener("keydown", this.handler);
+  },
+  unmounted() {
+    document.removeEventListener("keydown", this.handler);
+  },
+  /* watch: {
+    show(newVal) {
+      if (newVal) {
+        this.$nextTick(() => {
+          this.$refs.modal.focus();
+        });
+      }
+    },
+  }, */
+};
+</script>
+<!-- <style scoped>
+.modal-dialog {
+  outline: 0;
+}
+</style> -->
