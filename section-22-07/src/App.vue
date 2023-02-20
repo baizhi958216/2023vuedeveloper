2015329fe0f31f573d28f6b8d524eaaf1b17d54a
baizhi958216 <1475289190@qq.com>
2023-02-21 01:22:44 +0800

    22-007: The Teleport Component
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-22-07/src/App.vue
+++ section-22-07/src/App.vue
@@ -1,147 +1,16 @@
 <template>
   <div id="app">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <div class="mt-2 ml-2">
-      <button type="button" class="btn btn-primary" @click="open = true">
-        Open
-      </button>
-    </div>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-      sagittis, nibh a consectetur commodo, lacus nulla vestibulum lacus, porta
-      scelerisque eros lectus et metus. Aliquam tincidunt luctus mattis. Sed
-      ultricies justo sodales dolor facilisis fringilla. Praesent ut malesuada
-      est. Cras pharetra rhoncus sem, quis elementum arcu aliquam et.
-      Pellentesque sagittis elementum eros, sit amet aliquet arcu feugiat ac. In
-      ex augue, aliquet vehicula augue eget, semper commodo ex. In quis lobortis
-      eros. Etiam eget lacus ac leo tempor malesuada cursus in lectus. Curabitur
-      a iaculis risus. Phasellus sed rutrum tellus. Praesent porttitor tincidunt
-      rutrum. Donec et ipsum eu risus molestie feugiat non in enim.
-    </p>
-    <app-modal :show="open" @hide="open = false" />
+    <app-action />
   </div>
 </template>
 
 <script>
-import AppModal from "@/components/Modal.vue";
+import AppAction from "@/components/Action.vue";
 
 export default {
   name: "App",
   components: {
-    AppModal,
-  },
-  data() {
-    return {
-      open: true,
-    };
+    AppAction,
   },
 };
 </script>
---
+++ section-22-07/src/components/Action.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mt-2 ml-2" style="opacity: 0.5">
+    <button type="button" class="btn btn-primary" @click="open = true">
+      Open
+    </button>
+
+    <teleport to="body">
+      <app-modal :show="open" @hide="open = false" />
+    </teleport>
+  </div>
+</template>
+
+<script>
+import AppModal from "@/components/Modal.vue";
+
+export default {
+  name: "AppAction",
+  components: {
+    AppModal,
+  },
+  data() {
+    return {
+      open: true,
+    };
+  },
+};
+</script>
