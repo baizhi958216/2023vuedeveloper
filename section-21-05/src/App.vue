fdfe43a219a6277d6db05d229d898988870858f3
baizhi958216 <1475289190@qq.com>
2023-02-20 15:15:55 +0800

    21-005: Import Resources
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-21-05/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>{{ num }}</div>
+  <button type="button" @click.prevent="increment">Click Me</button>
+
+  <p>{{ name }}</p>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let num = ref(0);
+
+    console.log(num);
+
+    function increment() {
+      num.value++;
+    }
+
+    const user = reactive({
+      name: "John",
+      age: 20,
+    });
+
+    setTimeout(() => {
+      user.name = "Luis";
+    }, 3000);
+
+    return {
+      num,
+      increment,
+      ...toRefs(user),
+    };
+  },
+};
+</script>
