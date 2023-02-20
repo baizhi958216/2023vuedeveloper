0706f8a79b6405a93b6fcd56e17e26f3ce2b94cf
baizhi958216 <1475289190@qq.com>
2023-02-20 21:12:37 +0800

    21-012: Pinia Hooks
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-21-12/src/views/About.vue
+++ section-21-12/src/views/About.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="about">
     <h1>{{ $route.params.member }}</h1>
+    <p>{{ store.counter }}</p>
+    <p>{{ store.doubleCount }}</p>
+    <button type="button" @click.prevent="store.increment">Press me!</button>
   </div>
 </template>
 <script>
 import { useRoute, useRouter } from "vue-router";
+import { useCounterStore } from "../stores/counter";
 export default {
   setup() {
     const route = useRoute(); //this.$route
     const router = useRouter();
+    const store = useCounterStore();
 
     console.log(route.params.member);
 
     router.push({
       hash: "#test",
     });
+
+    return { store };
   },
 };
 </script>
