e07eb7aa719d1071c513ad9d281075cd0593e9ee
baizhi958216 <1475289190@qq.com>
2023-02-20 21:08:27 +0800

    21-012: Import Resources
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-21-12/src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>{{ $route.params.member }}</h1>
+  </div>
+</template>
+<script>
+import { useRoute, useRouter } from "vue-router";
+export default {
+  setup() {
+    const route = useRoute(); //this.$route
+    const router = useRouter();
+
+    console.log(route.params.member);
+
+    router.push({
+      hash: "#test",
+    });
+  },
+};
+</script>
