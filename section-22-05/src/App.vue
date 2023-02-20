7a8108c548f999fab9010431f0583de0836026dc
baizhi958216 <1475289190@qq.com>
2023-02-21 00:19:51 +0800

    22-005: Import Resources
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-22-05/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="mt-2 ml-2">
+      <button type="button" class="btn btn-primary" @click="open = true">
+        Open
+      </button>
+    </div>
+
+    <app-modal :show="open" @hide="open = false" />
+  </div>
+</template>
+
+<script>
+import AppModal from "@/components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppModal,
+  },
+  data() {
+    return {
+      open: false,
+    };
+  },
+};
+</script>
+
+<style src="bootstrap/dist/css/bootstrap.css"></style>
---
+++ section-22-05/src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="modal" :style="{ display: show ? 'block' : 'none' }">
+    <div class="modal-dialog" style="z-index: 2000">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Awesome Title</h5>
+          <button type="button" class="close" @click="close">
+            <span>&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Awesome content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop show"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppModal",
+  props: ["show"],
+  methods: {
+    close() {
+      this.$emit("hide");
+    },
+  },
+};
+</script>
