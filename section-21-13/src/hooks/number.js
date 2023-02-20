7f714c352eefaf7ef764d2942ed713eedda3bb9c
baizhi958216 <1475289190@qq.com>
2023-02-20 21:27:03 +0800

    21-013: Verifying Reactivity
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- section-21-13/src/hooks/number.js
+++ section-21-13/src/hooks/number.js
@@ -1,6 +1,13 @@
-import { ref, computed } from "vue";
+import { ref, computed, isRef, isReactive, reactive } from "vue";
 export const useNumber = () => {
   let num = ref(0);
+  const accounts = reactive({
+    checking: 3242,
+    savings: 242,
+  });
+
+  console.log(isRef(num));
+  console.log(isReactive(accounts));
 
   console.log(num);
 
