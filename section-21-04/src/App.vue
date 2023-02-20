25f7f861e0c19ca116c4a4342212dc54efa3c514
baizhi958216 <1475289190@qq.com>
2023-02-20 14:53:59 +0800

    21-004: Import Resources
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ section-21-04/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{ num }}</div>
+  <button type="button" @click.prevent="increment">Click Me</button>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let num = ref(0);
+
+    console.log(num);
+
+    function increment() {
+      num.value++;
+    }
+
+    return {
+      num,
+      increment,
+    };
+  },
+};
+</script>
